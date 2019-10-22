49ec73b9c7b9c9c3a31fff8108570ad5c84165fb
hoang tung son <hoangtungson164@gmail.com>
2019-10-22 10:00:22 +0700

    commit project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ColorPickComponent } from './color-pick/color-pick.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ColorPickComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/color-pick/color-pick.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-color-pick',
+  templateUrl: './color-pick.component.html',
+  styleUrls: ['./color-pick.component.css']
+})
+export class ColorPickComponent implements OnInit {
+
+  background = '#00e067';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onChange(value) {
+    this.background = value;
+  }
+}
